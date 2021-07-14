e5d0e2705a2af3d56e1471f79548bb21a907c37b
rajdeepdodiya <rajdodiya216@gmail.com>
2021-07-14 15:24:59 -0400

    Final changes.

--- js/highscores.js
+++ js/highscores.js
@@ -1,14 +1,107 @@
-
 var audio = document.getElementById('audio')
-audio.play();
+var isAudioPreferred;
+var highScoresList = new Array();
+const highScoresName  = "HighScores";
 
-const toggleAudio = () => {
-    
-    let image = document.querySelector(".audioButton").src
-    console.log(image);
-    audio.paused ? document.querySelector(".audioButton").src="assets/sound_muted.png" : document.querySelector(".audioButton").src="assets/sound_on.png" ;
-    audio.paused ? audio.play() : audio.pause();
+const getHighScoresFromLocalDb = () => {
+
+    // const scoresList = {"Rd", 1};
+    // localStorage.setItem("HighScores", scoresList);
+
+    if("HighScores" in localStorage){
+
+        const scoresList = JSON.parse(localStorage.getItem("HighScores"));
+        console.log("SCORES: "+list);
+
+        document.querySelectorAll("highScoresContainer").innerHTML = `<div>
+        <tr>
+        <th> Rank </tr>
+        <th> NAME </tr>
+        <th> SCORE </tr>
+        </tr>       
+        </div>
+        `
+
+        for(let i = 0 ; i < scoresList.size(); i++){
+
+            document.querySelector("#highScoresContainer").innerHTML += `<div>
+            <tr>
+            <td> <p> `+i+`. </p> </td>
+            <td> <p> `+scoresList[i].getName+` </p> </td>
+            <td> <p> `+scoresList[i].getScore+` </p> </td>
+            </tr>
+            </div>
+            `
+        }
+
+    }
+
+    else{
+        console.log("No scores saved");
+        
+        document.querySelector(".highScoresContainer").innerHTML += `<div>
+        <p>NO SCORES EXIST IN DATABASE.  </p>
+        </div>` 
+    }
 
 }
 
+const updateSoundIcon = () => {
+    
+    let image = document.querySelector(".audioButton").src
+    console.log(image);
+
+    if(isAudioPreferred == "true"){
+        document.querySelector(".audioButton").src="assets/sound_muted.png"
+    }
+    else{
+        document.querySelector(".audioButton").src="assets/sound_on.png"
+    }
+
+}
+
+const toggleAudio = () => {
+    
+    audio.paused ? audio.play() : audio.pause();
+    
+    if(audio.paused){
+
+        isAudioPreferred = "false"
+        localStorage.setItem("isAudioPreferred", "false");
+    }
+
+    else{
+
+        isAudioPreferred = "true"
+        localStorage.setItem("isAudioPreferred", "true");
+    }
+
+    updateSoundIcon();
+    console.log(localStorage.getItem("isAudioPreferred"));
+
+}
+
+const getAudioPreferences = () => {
+
+    isAudioPreferred = localStorage.getItem("isAudioPreferred");
+    console.log("getAudioPreferences : isAudioPreferred : "+isAudioPreferred);
+
+    //true
+    if(isAudioPreferred == "true"){
+        console.log("getAudioPreferences(): playing audio");
+        audio.play();
+    }
+    //false
+    else{
+        console.log("getAudioPreferences(): not playing audio");
+        audio.pause();
+    }
+    updateSoundIcon();
+    getHighScoresFromLocalDb();
+}
+
 document.querySelector("#audio-button").addEventListener("click", toggleAudio);
+window.onload = getAudioPreferences
+
+
+// document.addEventListener('load', getAudioPreferences)
--- js/homepage.js
+++ js/homepage.js
@@ -1,13 +1,78 @@
 var audio = document.getElementById('audio')
+var isAudioPreferred;
 
-const toggleAudio = () => {
+const updateSoundIcon = () => {
     
     let image = document.querySelector(".audioButton").src
     console.log(image);
-    audio.paused ? document.querySelector(".audioButton").src="assets/sound_muted.png" : document.querySelector(".audioButton").src="assets/sound_on.png" ;
-    audio.paused ? audio.play() : audio.pause();
 
+    if(isAudioPreferred){
+        document.querySelector(".audioButton").src="assets/sound_muted.png"
+    }
+    else{
+        document.querySelector(".audioButton").src="assets/sound_on.png"
+    }
+
+}
+
+const toggleAudio = () => {
+    
+    audio.paused ? audio.play() : audio.pause();
+    
+    if(audio.paused){
+        isAudioPreferred = false
+        localStorage.setItem("isAudioPreferred", false);
+    }
+
+    else{
+
+        isAudioPreferred = true
+        localStorage.setItem("isAudioPreferred", true);
+    }
+
+    updateSoundIcon();
+
+}
+
+
+const getAudioPreferences = () => {
+
+    isAudioPreferred = localStorage.getItem("isAudioPreferred");
+    console.log("getAudioPreferences : isAudioPreferred : "+isAudioPreferred);
+
+    //true
+    if(isAudioPreferred == "true"){
+        console.log("getAudioPreferences(): playing audio");
+        audio.play();
+    }
+    //false
+    else{
+        console.log("getAudioPreferences(): not playing audio");
+        audio.pause();
+    }
+    updateSoundIcon();
+}
+
+const saveCurrentUserToLocalStorage = () =>{
+
+   const playerName = document.getElementById("playerName").value.trim();
+
+    if(playerName === ""){
+        console.log("No name entered");
+        alert(`Please enter your name to start playing`);
+    }
+
+    else{
+        localStorage.setItem("currentUser", playerName);
+        console.log(playerName);
+        window.open("Gameplay.html", "_self");
+    }
 }
 
 document.querySelector("#audio-button").addEventListener("click", toggleAudio)
 
+document.getElementById("startGame").addEventListener("click", saveCurrentUserToLocalStorage)
+
+// document.addEventListener("load", getAudioPreferences)
+
+window.onload = getAudioPreferences
